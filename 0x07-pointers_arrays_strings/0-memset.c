6504e60435cd91a729212ce060b43496d575b904
wangenga <daisyimg65@gmail.com>
2022-07-18 14:50:15 -0700

    task 0

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_memeset - main function
+ * @s: pointer to buffer
+ * @b: constant byte
+ * @n: size of the buffer to fil
+ * Description: fills a memeory with a constant byte
+ * Return: pointer to memory s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+		s[i] = b;
+		i++;
+	}
+	return (s);
+}
