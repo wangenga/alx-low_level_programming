ed71c0ab0cdd159daadf1063f5c32e2af927e75b
wangenga <daisyimg65@gmail.com>
2022-07-18 15:26:06 -0700

    task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _strspn - main function
+ * @s: pointer to the string
+ * @accept: character to locatein the string
+ * Return: number of byte in the intitial s ,bytes from accept
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	while (*s)
+	{
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (!c ? s : NULL);
+}
