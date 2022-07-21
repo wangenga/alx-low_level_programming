4a508844cb5a86e9b39f7594fb9cb7cbdedafa11
wangenga <daisyimg65@gmail.com>
2022-07-20 17:05:58 -0700

    task 2

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * wildcmp - function
+ * @s1: variable
+ * @s2: variable
+ * Return: Int
+ */
+int wildcmp(char *s1, char *s2)
+{
+	if (*s1 == *s2 && *s1 != '\0' && *s2 != '\0')
+		return (wildcmp(s1 + 1, s2 + 1));
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	if (*s2 == '*')
+	{
+		if (wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+		if (*(s1 + 1) != '\0' && *s2 == '\0')
+		       return (0);
+	}
+return (0);
+}
